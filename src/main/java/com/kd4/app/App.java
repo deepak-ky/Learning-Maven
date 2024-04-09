d4c75441d1edfe1e03a9a779ee14d6e822e6c6d1
Deepak Kumar <deepak.ky.delhi@gmail.com>
2024-04-09 21:15:43 +0530

    initial-commit

---
+++ src/main/java/com/kd4/app/App.java
@@ -0,0 +1,13 @@
+package com.kd4.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
